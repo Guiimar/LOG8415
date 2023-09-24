f0a598de55231ca62a43f5e1116788fc33041013
Guiimar <guillaume.martin2@imt-atlantique.net>
2023-09-24 18:48:23 -0400

    add rpreliminary create_instance

---
+++ TP1/main.py
@@ -0,0 +1,40 @@
+import boto3
+
+def create_instance_ec2(num_instances,ami_id,instance_type,key_pair_name,resource,count,security_group_id=None):
+    instances=[]
+    for i in range(num_instances):
+        instances.append(resource.create_instances(
+            ImageId=ami_id,
+            InstanceType=instance_type,
+            KeyName=key_pair_name["KeyName"],
+            MinCount=1,
+            MaxCount=1,
+            SecurityGroupIds=[security_group_id] if security_group_id else [],
+            TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': 'lab1-ec2-instance-' + str(i + 1)
+                            },
+                        ]
+                    },
+                ]
+        ))
+        print(f'{instances[i]} is starting')
+    return instances
+
+def create_target_group(group_name,vpc_id):
+    tg_response=client.create_target_group(
+        Name=group_name,
+        Protocol='HTTP',
+        Port=80,
+        VpcId=vpc_id
+    )
+    target_group_arn = tg_response["TargetGroups"][0]["TargetGroupArn"]
+    #Register target?
+    
+    return target_group_arn
+
+
