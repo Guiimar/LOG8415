ec86d2697f0b4d5f36be7b4cc2740a948cb1791a
Guiimar <guillaume.martin2@imt-atlantique.net>
2023-09-24 17:19:46 -0400

    create flask_app

---
+++ TP1/flask_app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/cluster1")
+
+def cluster1_route():
+    return ("Cluster number 1 is responding now !")
+
+@app.route("/cluster2")
+
+def cluster2_route():
+    return ("Cluster number 2 is responding now !")
+
+if __name__=='__main__':
+    app.run(debug=True)
