c2adb9a5122ea53cf6069e8d20a891b159ea3354
MBelch <mehdi.belchiti@gmail.com>
2023-09-28 16:28:31 -0400

    debugging, get VPC_iD and sg_id as default using client conncetion

--- TP1/main.py
+++ TP1/main.py
@@ -15,6 +15,19 @@ import configparser
 import boto3
 import time
 
+# Define a client connection :
+def client_connection_ec2(aws_access_key_id, aws_secret_access_key, aws_session_token):
+    ec2 =  boto3.client('ec2',
+                       'us-east-1',
+                       aws_access_key_id= aws_access_key_id,
+                       aws_secret_access_key=aws_secret_access_key ,
+                      aws_session_token= aws_session_token) 
+    print("\n\n===================> Client connection <======================\n\n")
+    print("\n\tClient connection has been made succesfuly!!!!\n")
+    print("\n\n==============================================================")
+
+    return(ec2)
+
 #Function to establish connection with AWS CLI credentials: 
 def create_connection_ec2(aws_access_key_id, aws_secret_access_key, aws_session_token):
     ec2 =  boto3.resource('ec2',
@@ -80,8 +93,8 @@ def create_instance_ec2(num_instances,ami_id,
     return instances
 
 #Function to create target groups : 
-def create_target_group(targetname,vpc_id,port):
-    tg_response=client.create_target_group(
+def create_target_group(targetname,vpc_id,port, resource):
+    tg_response=resource.create_target_group(
         Name=targetname,
         Protocol='HTTP',
         Port=port,
@@ -174,6 +187,8 @@ if __name__ == '__main__':
         access_key = config_object.get("resource","aws_secret_access_key")
         session_token = config_object.get("resource","aws_session_token")
         ami_id = config_object.get("ami","ami_id")
+
+    key_pair_name = "vockey"
         
     """
     key_id="ASIA37YNGERBT6X6LQTC"
@@ -181,31 +196,74 @@ if __name__ == '__main__':
     session_token="FwoGZXIvYXdzEKr//////////wEaDBO5ze9K0vRG+ashZCLOAU0/95fFC+tjNFPeM7QOEnhtztXUO0eZZytXAEulebfcWHep2MZKlMhgjsqtOGxS1hZ7HfvoXW9bsTwMVnT3DBavYP6PPINFXQjexeEegaalIRXaKKwufyF6feVMVH6XkXRxqY6E1Tc7/yJwaO3nR5hqjVj+SRgqjY7K8pzA7/gaxopW6nt8Xu/M5XlRq951SIlps2YKxyesvYsEud0uGlDztap7uyR7kEbDdUJwDYVShJuRnRgYR45n7K3C25xGQZdRhHrgjXZ234BewLBQKI+40agGMi1+WsdAzD767xhcOfSXY6FCqB7ZGdHFdVEMq6ASBqlAc+Zp7dtDqBqwT2PYHko="
     ami_id ='ami-03a6eaae9938c858c'
     """
+    # Create a client connection mainly to get the standard Vpc ID :
+    client = client_connection_ec2(key_id, access_key, session_token)
 
+    # Get the first VPC description dict : 
+    response = client.describe_vpcs()
+    vpc_desc = response.get("Vpcs")
+    #print(vpc_desc)
+
+    # Get the first VPC id in the list of the dict returned by vpc_responce : 
+    vpc_id = vpc_desc[0].get('VpcId')
+    #print(vpc_id)
+
+    
     # Connection created : 
     ec2 = create_connection_ec2(key_id, access_key, session_token)
-
-    client = boto3.client('elbv2', region_name='us-east-1',aws_access_key_id= key_id,
-                       aws_secret_access_key=access_key ,
-                      aws_session_token= session_token)
-    print("\n\n Connection made succefuly \n\n")
-
-    key_pair_name = "vockey"
-
+    
+    
+    # The step is to perform later on :
+    """
     # Create Vpc : 
-    vpc_id=create_vpc('172.31.0.0/16',ec2)
-    print(vpc_id)
+    vpc_id= create_vpc('172.31.0.0/16',ec2)
+    print("\n\n"+vpc_id+"\n\n")
+    """
+    
+# ==========================================================================================================
+    # =============> Pensez plutôt a recupérer le security_group par default avec les instructions ci-dessous
+    #===========================================================================================================
+    #Get the standard security group from the standard VPC :
+
+    sg_dict = client.describe_security_groups(Filters=[
+        {
+            'Name': 'vpc-id',
+            'Values': [
+                vpc_id,
+            ]
+        },
+
+    {
+            'Name': 'group-name',
+            'Values': [
+                "default",
+            ]
+        },
+    
+    ])
+
+    security_group_id = (sg_dict.get("SecurityGroups")[0]).get("GroupId")    
+    #print(security_group_id)
+
+    # Creation of a security group is to perform later on
+    """
+    security_group_id = (sg_dict.get("SecurityGroups")[0]).get("GroupId")    
+    #print(security_group_id)
     # Create security group for vpcId : 
-    security_group_id =create_security_group('Security_group_created_for_lab','Security_group',vpc_id,ec2)
-    print(security_group_id)
+    security_group_id = create_security_group('Security_group_created_for_lab','Security_group',vpc_id,ec2)
+    print("\n"+security_group_id+"\n")"""
+    
+    
     subnets=['subnet-053bd769717aa1641','subnet-00aebad3742819994']
 
+    
     # Create 4 instances with t2.large as intance type: 
     instance_type = "t2.large"
     print("\n\n creating instances, type : t2.large\n\n")
     instances_t2= create_instance_ec2(3,ami_id, instance_type,key_pair_name,ec2,security_group_id)
     print(instances_t2)
     print("\n\n instances created succefuly instance type : t2.large")
+
 
     # Create 4 instances with m4.large as instance type:
     instance_type = "m4.large"
@@ -215,8 +273,8 @@ if __name__ == '__main__':
     print("\n\n instances created succefuly instance type  : m4.large")
 
     #Creaye the two targets groups (Clusters)
-    target_group_1=create_target_group('TargetGroup1',vpc_id,80)
-    target_group_2=create_target_group('TargetGroup2',vpc_id,8080)
+    target_group_1=create_target_group('TargetGroup1',vpc_id,80, ec2)
+    target_group_2=create_target_group('TargetGroup2',vpc_id,8080, ec2)
 
     time.sleep(120)
 
@@ -240,8 +298,10 @@ if __name__ == '__main__':
     rule_list_2=create_listener_rule(listener_group2,target_group_2,'/cluster2')
     print('Règles créées')
 
+    """
     #Terminate EC2 instances when not needed
     total_instances=instances_t2+instances_m4
     time.sleep(160)
     terminate_instances(total_instances,ec2)
     print("Instances terminated")
+    """
