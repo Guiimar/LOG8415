a3918cf74dd20c4f618e731f93f2478f5e7a2788
MBelch <mehdi.belchiti@gmail.com>
2023-10-01 19:03:29 -0400

    Add module of functions used in the main progrom and modifying Flask

--- TP1/flask_app.py
+++ TP1/flask_app.py
@@ -3,16 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/cluster1")
-@app.route("/cluster2")
-
-def cluster1_route():
-    return ("Cluster number 1 is responding now !")
-
-@app.route("/cluster2")
-
-def cluster2_route():
-    return ("Cluster number 2 is responding now !")
+def my_app():
+    return 'Cluster 1 Instance 1 responding'
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
