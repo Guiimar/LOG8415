026f0650fe6e9f0987c36a560154a13139487c54
Guillaume Martin <98980842+Guiimar@users.noreply.github.com>
2023-10-05 20:08:31 -0400

    modify scripts,  modify section credentials

--- TP1/scripts.sh
+++ TP1/scripts.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 git clone https://github.com/Guiimar/LOG8415.git
 cd LOG8415/TP1
+# Define new AWS credentials
+new_aws_access_key=""
+new_aws_secret_key="NouvelleCleSecrete"
+new_aws_token_key=""
+# Replace the old AWS credentials with new values
+sed -i "s/aws_access_key_id=.*/aws_access_key_id=${new_aws_access_key}/" credentials.ini
+sed -i "s/aws_secret_access_key=.*/aws_secret_access_key=${new_aws_secret_key}/" credentials.ini
+sed -i "s/aws_session_token=.*/aws_session_token=${new_aws_token_key}/" credentials.ini
 docker build -t log8415 .
 docker run -it log8415
