54e4617cc39e170e82d559f38cf07c4784d786e1
Guillaume Martin <98980842+Guiimar@users.noreply.github.com>
2023-10-05 20:10:13 -0400

    replace aws keys in scripts.sh

--- TP1/scripts.sh
+++ TP1/scripts.sh
@@ -2,9 +2,9 @@
 git clone https://github.com/Guiimar/LOG8415.git
 cd LOG8415/TP1
 # Define new AWS credentials
-new_aws_access_key=""
-new_aws_secret_key="NouvelleCleSecrete"
-new_aws_token_key=""
+new_aws_access_key="youraccesskey"
+new_aws_secret_key="yoursecretkey"
+new_aws_token_key="yourtokenkey"
 # Replace the old AWS credentials with new values
 sed -i "s/aws_access_key_id=.*/aws_access_key_id=${new_aws_access_key}/" credentials.ini
 sed -i "s/aws_secret_access_key=.*/aws_secret_access_key=${new_aws_secret_key}/" credentials.ini
