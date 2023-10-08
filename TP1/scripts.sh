a37eabfcb0fe60dc39db8a4dd2fed3dd8cf5b7c3
Zakaria Haniri <132126523+ZakiHANI@users.noreply.github.com>
2023-10-07 23:45:06 -0400

    Update scripts.sh

--- TP1/scripts.sh
+++ TP1/scripts.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
+# Clone the git repository for the assignement 
 git clone https://github.com/Guiimar/LOG8415.git
+# Open folder TP1
 cd LOG8415/TP1
-# Define new AWS credentials
+# Enter your AWS credentials here
 new_aws_access_key="key"
 new_aws_secret_key="secret"
 new_aws_token_key="token"
 
-# Pour aws_access_key_id
+# Fill in the credentials.ini file with entered aws_access_key
 sed -i "s#aws_access_key_id=.*#aws_access_key_id=${new_aws_access_key}#" credentials.ini
 
-# Pour aws_secret_access_key
+# Fill in the credentials.ini file with entered new_aws_secret_key
 sed -i "s#aws_secret_access_key=.*#aws_secret_access_key=${new_aws_secret_key}#" credentials.ini
 
-# Pour aws_session_token
+# Fill in the credentials.ini file with entered new_aws_token_key
 sed -i "s#aws_session_token=.*#aws_session_token=${new_aws_token_key}#" credentials.ini
 
-cp credentials.ini docker/
+# Copy credentials.ini file in Docker folder
+cp credentials.ini Docker/
 
-# Install requirements from requirements.txt
-
+# Install boto3
 pip install boto3
 
-#Execute la création des instances
+#Execute Setup python file
 cd Setup
 python3 Setup_main.py
 
+# Open the folder Docker
 cd ..
-
 cd Docker
 
+# Build Docker container
 docker build -t test .
+# Run container (containing Benchemark python file)
 docker run -it test
